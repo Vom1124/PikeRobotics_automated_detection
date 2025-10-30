b1af131b1ccfe7808ef1f5012f5640d4b8715526
Vom <129146507+Vom1124@users.noreply.github.com>
2025-10-29 20:54:44 -0500

    Delete build directory

--- build/automated_counter/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vom/ros2_ws/PikeRobotics_automated_counting/install/automated_counter'
--- build/automated_counter/share/automated_counter/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/vom/ros2_ws/PikeRobotics_automated_counting/build/automated_counter"
